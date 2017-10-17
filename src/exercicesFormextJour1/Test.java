f89feeca5aa65b0cc708e4704c01022c5818cd4a
Arez <arez.formext@laposte.net>
2017-10-17 14:08:23 +0200

    add a modified Test.java file

--- src/exercicesFormextJour1/Test.java
+++ src/exercicesFormextJour1/Test.java
@@ -15,8 +15,16 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO It starts all code, add methods and other classes must be call from here
 		// try to be clear and simple
-		System.out.println("coucou");
+		System.out.println("Hello world");
+		System.err.println("coucou l'erreur");
+		affiche("Bonjour");
+		affiche("à tous");
+		
 
+	}
+	
+	public static void affiche(String texte) {
+		System.out.println(texte);
 	}
 
 }
