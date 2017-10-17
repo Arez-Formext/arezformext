606964b1334d53eb1c26bb218f6790bc5c0e594e
Arez <arez.formext@laposte.net>
2017-10-17 10:30:15 +0200

    Inital commit for the repository

---
+++ src/exercicesFormextJour1/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package exercicesFormextJour1;
+
+/**
+ * @author dev
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO It starts all code, add methods and other classes must be call from here
+
+	}
+
+}
