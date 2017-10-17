6f6ee6369faab34747df506d2a261b32ffb6fa00
Arez <arez.formext@laposte.net>
2017-10-17 10:37:59 +0200

    modification file for test

--- src/exercicesFormextJour1/Test.java
+++ src/exercicesFormextJour1/Test.java
@@ -14,6 +14,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO It starts all code, add methods and other classes must be call from here
+		// try to be clear and simple
 
 	}
 
